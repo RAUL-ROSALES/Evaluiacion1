d334e39104e667f0e7b8d5635efbb218fde13ec7
RAUL-ROSALES <55122080+RAUL-ROSALES@users.noreply.github.com>
2019-11-27 23:00:58 -0800

    Add files via upload

---
+++ Evaluacion1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operacion o = new Operacion();
+          
+        }
+    }
+}
